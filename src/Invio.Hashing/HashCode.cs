84dc23b15ab52dd03331cba57454937b5c00b445
Brian Caruso <carusobp@gmail.com>
2016-09-06 14:25:00 -0700

    First commit of basic HashCode generator implementation

---
+++ src/Invio.Hashing/HashCode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Invio.HashCode {
+
+    public static class HashCode {
+
+        // These constants has no significance other than they are prime,
+        // limiting the liklihood that they will create hash codes that
+        // are the same for distinct objects.
+
+        private const int basePrime = 17;
+        private const int iterationPrime = 23;
+        private const int nullPrime = 31;
+
+        public static int From(params object[] values) {
+            return HashCode.From((IEnumerable<object>)values);
+        }
+
+        public static int From(IEnumerable<object> values) {
+            unchecked {
+                var hash = basePrime;
+
+                foreach (var value in values) {
+                    hash *= iterationPrime;
+
+                    if (value == null) {
+                        hash += nullPrime;
+                    } else {
+                        hash += value.GetHashCode();
+                    }
+                }
+
+                return hash;
+            }
+        }
+
+    }
+
+}
